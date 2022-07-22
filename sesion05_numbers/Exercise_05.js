a401e12b4bf4f8306430fe53ab96a0cfa29d0410
christian lazo <kenishi.cristianxd@gmail.com>
2022-07-21 20:21:37 -0500

    Section 05 Ok

---
+++ sesion05_numbers/Exercise_05.js
@@ -0,0 +1,6 @@
+let heightCm = 170;
+let heightMe = 1.7;
+let weightKg = 83;
+let heightMeRound = Math.ceil(heightMe);
+let weightKgFloor = Math.floor(weightKg);
+let isMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
